8c3f43a5bb6b3100ab84df1c6349bcb2b2156195
TanujaShekhawat <143373856+TanujaShekhawat@users.noreply.github.com>
2023-08-29 10:31:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'connection.php';
+include 'topnav.php';
+?>
+
+          <!-- Breadcrumbs-->
+           
+          </ol>
+           <div class="Student_logo">
+            <a href="#" class="image full"><img src="image/n3.jpg" style="width:1000px;" style="background-attachment: fixed;" style="background-size: contain;"  > </a>
+          <!-- Page Content -->
+          <h4></h4>
+          <hr> 
+          <p> <h5>  </h5></p>
+<?php include 'footer.php'; ?>
